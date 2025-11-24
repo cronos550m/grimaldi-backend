b88830932c79c054fd748d2470f3ac68512e2c25
cronos550m <cronos550m@gmail.com>
2025-11-24 01:42:59 -0300

    3.0

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use((err, req, res, next) => {
   res.status(err.status || 500).json({ error: err.message || 'Server error' });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 const prisma = new PrismaClient();
 
 async function main(){
